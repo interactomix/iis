09a5a128db82a102166cb16c69239a288f340255
Maximilian Friedersdorff <maxf130@gmail.com>
2016-09-27 14:05:09 +0100

    Set up Flask-Mail to use local smtpd

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -8,6 +9,8 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Load Flask-Mail
+mail = Mail(app)
 
 
 
