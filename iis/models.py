caa0f50e70513647d80802301b492a14181faadb
Maximilian Friedersdorff <maxf130@gmail.com>
2016-10-05 16:50:13 +0100

    Add 'Computation' model

--- iis/models.py
+++ iis/models.py
@@ -25,3 +25,17 @@ class User(Model, UserMixin):
                        server_default='0')
     first_name = db.Column(db.String(100), nullable=False, server_default='')
     last_name = db.Column(db.String(100), nullable=False, server_default='')
+
+
+class Computation(Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    process_uid = db.Column(db.String(100), nullable=False, unique=True)
+    pdf = db.Column(db.Text(), nullable=True, unique=False)
+    input_data = db.Column(db.Text())
+    output_data = db.Column(db.Text())
+    status = db.Column(db.Enum("processing",
+                               "interrupted",
+                               "finished",
+                               "stopped"))
+    progress = db.Column(db.Integer())
