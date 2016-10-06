5beee8ed76850557c1aa85ac71a1e62ef6d2902a
Maximilian Friedersdorff <maxf130@gmail.com>
2016-10-06 13:32:07 +0100

    Introduce logging
    This is *essential* for debugging multiprocessing
    applications with daemons.  The daemons cannot print
    debugging messages to the screen

--- configuration/development.py
+++ configuration/development.py
@@ -1,10 +1,12 @@
 import pathlib
+import logging
 
 _basedir = pathlib.Path(__file__).parents[1]
 
 SQLALCHEMY_DATABASE_URI = (
     'sqlite:///' + str(_basedir.joinpath(pathlib.PurePath('app.db')).resolve())
 )
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = 'INSECURE'
 
@@ -12,4 +14,32 @@ MAIL_SERVER = 'localhost'
 MAIL_PORT = '25'
 MAIL_DEFAULT_SENDER = 'no-reply@localhost.localdomain'
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": '%(asctime)s %(levelname)s: %(message)s '
+                      '[in %(pathname)s:%(lineno)d]'
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "formatter": "verbose",
+            "class": "logging.FileHandler",
+            "filename": "iis.log"
+        },
+    },
+    "loggers": {
+        "iis": {
+            "level": "DEBUG",
+            "handlers": ["file"]
+        },
+    }
+}
+LOGGER_NAME = "iis"
+
+
 del pathlib
+del logging
--- iis/__init__.py
+++ iis/__init__.py
@@ -1,3 +1,5 @@
+import logging.config
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,6 +13,11 @@ def create_app(config: object) -> Flask:
     app = Flask(__name__)
     app.config.from_envvar('IIS_FLASK_SETTINGS')
     app.config.from_object(config)
+
+    # Call app.logger to prevent it from clobbering configuration
+    app.logger
+    logging.config.dictConfig(app.config['LOGGING'])
+    app.logger.info("App configured.")
     return app
 
 app = create_app(None)
@@ -30,6 +37,7 @@ from iis.models import User  # noqa: E402
 
 db_adapter = SQLAlchemyAdapter(db, User)
 user_manager = UserManager(db_adapter, app)
+
 
 
 from iis import views, models  # noqa: E402, F401
