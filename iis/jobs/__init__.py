0b7c0c727b3b56cc20b90da90732fae28aaaf479
Maximilian Friedersdorff <maxf130@gmail.com>
2016-10-17 11:48:20 +0100

    Fix mypy complaint about unknown type

--- iis/jobs/__init__.py
+++ iis/jobs/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 
-jobs = Blueprint('jobs', __name__, template_folder='./templates')
+jobs = Blueprint('jobs', __name__,
+                 template_folder='./templates')  # type: Blueprint
 
 from . import views, models # noqa: E402, F401
